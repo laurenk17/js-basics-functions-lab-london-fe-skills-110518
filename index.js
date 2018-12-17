133fca55e809d74c2373532235936e3b3ddc0fff
lauren kernan <lauren.kernan17@gmail.com>
2018-12-17 20:44:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function calculatesFarePrice(start, destination){
   if (feet <= 400) {
     return 0
   } else if(feet > 400) {
-   return (someValue *0.02) 
+   return (feet *0.02) 
   } else if(feet >2000 & feet < 2500){
    return 25
   } else if (feet > 2500) {
