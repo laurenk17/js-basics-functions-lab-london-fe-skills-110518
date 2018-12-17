290900b0fb996a15551c5ce4acaffefb13baed1f
lauren kernan <lauren.kernan17@gmail.com>
2018-12-17 20:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  if (someValue <= 42) {
+  return(42 - someValue) 
+  } else if (someValue > 42) {
+  return(someValue - 42)
+  }
+}
+
+function distanceFromHqinFeet(someValue) {
+  distanceFromHqInBLocks(someValue);
+  return(someValue * 264);
+}
+
+function distanceTravelledInFeet(startingBlock, EndingBlock) {
+  if (startingBlock < endingBlock) {
+    return (endingBlock - startingBlock) * 264 
+  }
+}
+
+function calculatesFarePrice(someValue){
+  distanceTravelledInFeet(someValue);
+  if (distanceTravelledInFeet <= 400) {
+    return 0
+  } else if(distanceTravelledInFeet > 400) {
+   return (someValue *0.02) 
+  } else if(distanceTravelledInFeet >2000 & distanceTravelledInFeet < 2500){
+   return 8
+  } else if (distanceTravelledInFeet > 2500) {
+    return ('cannot travel that far')
+  }
+}
+
+
